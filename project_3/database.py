6227fa4f83fe6c7fa38bea137c5103c7ee2d45c7
dtuskippy <achelko@gmail.com>
2026-05-11 16:39:05 +0200

    New value for database key

--- project_3/database.py
+++ project_3/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bark@localhost/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bark@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoapplicationdatabase_4xtg_user:cSNBPSvZVvjiuh2vpQmyZPu4MB6haN09@dpg-d80ugkrbc2fs738h2bl0-a/todoapplicationdatabase_4xtg'
+
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
