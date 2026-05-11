d8e1d0082a5a23ed20ebd37beef5595dcbc725a4
dtuskippy <achelko@gmail.com>
2026-05-11 16:46:07 +0200

    Latest database url change

--- project_3/database.py
+++ project_3/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 # SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bark@localhost/TodoApplicationDatabase'
-SQLALCHEMY_DATABASE_URL = 'postgresql://todoapplicationdatabase_4xtg_user:cSNBPSvZVvjiuh2vpQmyZPu4MB6haN09@dpg-d80ugkrbc2fs738h2bl0-a/todoapplicationdatabase_4xtg'
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:bark@localhost/TodoApplicationDatabase")
+
 
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
